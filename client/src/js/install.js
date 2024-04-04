0d5255d50af20bf92fc7e9844a4f7bbaa8a20f1e
TOAN NGUYEN <ttngbt06@gmail.com>
2024-04-04 18:18:10 -0500

    Fixing error

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -17,9 +17,10 @@ butInstall.addEventListener('click', async () => {
     if (!promptEvent) {
      return;
     }
-     promptEvent.prompt();
-     window.deferredPrompt = null;
-     butInstall.classList.toggle('hidden', true); 
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true); 
 });
 
 // TODO: Add an handler for the `appinstalled` event
